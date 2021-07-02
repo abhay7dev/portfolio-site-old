3c05318103012c3c74e92b4fb1877c002837c238
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2021-07-02 01:16:21 +0000

    Added nomodule support

---
+++ src/public/static/scripts/nomodule.js
@@ -0,0 +1,5 @@
+window.alert("Please switch to a *modern* browser which supports newer JavaScript standards");
+window.close();
+while(true) {
+	window.alert("PLEASE SWITCH TO A MODERN BROWSER");
+}
