809364a9a1fdd6aff3cb146b97b177f1f0973691
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2021-11-28 22:25:15 +0000

    I am a God. I updated the halo stuff

--- src/modules/data/halo.js
+++ src/modules/data/halo.js
@@ -1,53 +1,84 @@
-import fetch from "node-fetch";
+import fetchF from "node-fetch";
 import { halo } from "../../config.js";
 const { CRYPTUM_TOKEN, USER_ID } = halo;
 
-const getRand = (min = 1, max = 19) => {
+const rand = (min = 1, max = 19) => {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
-const fetchHDA = async (url) => {
-	return await (await fetch(`https://cryptum.halodotapi.com/games/hmcc${url}`, {
-		headers: {
-			"Content-Type": "application/json", 
-			Authorization: `Cryptum-Token ${halo.CRYPTUM_TOKEN}`
-		}
-	})).json();
+const fetch = {
+	mcc: async (url) => {
+		return await (await fetchF(`https://cryptum.halodotapi.com/games/hmcc${url}`, {
+			headers: {
+				"Content-Type": "application/json", 
+				Authorization: `Cryptum-Token ${halo.CRYPTUM_TOKEN}`
+			}
+		})).json();
+	},
+	infinite: async (url) => {
+		return await (await fetchF(`https://cryptum.halodotapi.com/games/hi${url}`, {
+			headers: {
+				"Content-Type": "application/json", 
+				Authorization: `Cryptum-Token ${halo.CRYPTUM_TOKEN}`
+			}
+		})).json();
+	},
+	image: async (url) => {
+		const img = await fetchF(url);
+		if(img.status === 200) return img.body;
+		return `/static/images/404.png`;
+	},
 }
 
 export default Object.freeze({
-	stats: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/stats/players/${username}/service-record`);
-	},
-	ranks: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/stats/players/${username}/ranks`);
-	},
-	matches: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/stats/players/${username}/recent-matches`);
-	},
-	xp: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/stats/players/${username}/xp`);
-	},
-	appearance: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/appearance/players/${username}`);
-	},
-	screenshots: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/media/players/${username}/screenshots`);
-	},
-	clips: async (username = "EpicGamer007313") => {
-		return await fetchHDA(`/media/players/${username}/clips`);
-	},
-	/* clip: (id) => {
-		return `https://cryptum.halodotapi.com/games/hmcc/media/clips/${USER_ID}-${id}`;
-	}, */
-	images: {
-		xp: async (username = "EpicGamer007313") => {
-			return (await fetch(`https://cryptum.halodotapi.com/tooling/cards/games/hmcc/stats/players/${username}/xp.jpg?v=1&bg=${getRand()}`)).body;
+	mcc: {
+		stats: async (username = "EpicGamer007313") => {
+			return await fetch.mcc(`/stats/players/${username}/service-record`);
 		},
 		ranks: async (username = "EpicGamer007313") => {
-			return (await fetch(`https://cryptum.halodotapi.com/tooling/cards/games/hmcc/stats/players/${username}/ranks.jpg?v=1&bg=${getRand()}`)).body;
+			return await fetch.mcc(`/stats/players/${username}/ranks`);
 		},
-	}
+		matches: async (username = "EpicGamer007313") => {
+			return await fetch.mcc(`/stats/players/${username}/recent-matches`);
+		},
+		xp: async (username = "EpicGamer007313") => {
+			return await fetch.mcc(`/stats/players/${username}/xp`);
+		},
+		appearance: async (username = "EpicGamer007313") => {
+			return await fetch.mcc(`/appearance/players/${username}`);
+		},
+		screenshots: async (username = "EpicGamer007313") => {
+			return await fetch.mcc(`/media/players/${username}/screenshots`);
+		},
+		clips: async (username = "EpicGamer007313") => {
+			return await fetch.mcc(`/media/players/${username}/clips`);
+		},
+		images: {
+			xp: async (username = "EpicGamer007313") => {
+				return await fetch.image(`https://cryptum.halodotapi.com/tooling/cards/games/hmcc/stats/players/${username}/xp.jpg?v=1&bg=${rand()}`);
+			},
+			ranks: async (username = "EpicGamer007313") => {
+				return await fetch.image(`https://cryptum.halodotapi.com/tooling/cards/games/hmcc/stats/players/${username}/ranks.jpg?v=1&bg=${rand()}`);
+			},
+		}
+	},
+	infinite: {
+		stats: async (username = "EpicGamer007313") => {
+			return await fetch.infinite(`/stats/players/${username}/service-record/global`);
+		},
+		matches: async (username = "EpicGamer007313") => {
+			return await fetch.infinite(`/stats/players/${username}/matches`);
+		},
+		appearance: async (username = "EpicGamer007313") => {
+			return await fetch.infinite(`/appearance/players/${username}`);
+		},
+		screenshots: async (username = "EpicGamer007313") => {
+			return await fetch.infinite(`/media/players/${username}/screenshots`);
+		},
+		clips: async (username = "EpicGamer007313") => {
+			return await fetch.infinite(`/media/players/${username}/clips`);
+		},	
+	},
 });
 
 /**
--- src/modules/routers/api/halo.js
+++ src/modules/routers/api/halo.js
@@ -1,83 +1,38 @@
 import express from "express";
 const router = express.Router();
 
-import mcc from "../../data/halo.js";
+import halo from "../../data/halo.js";
 
-router.use((req, res, next) => {
-	res.mccJson = async (func = req.path.substring(1)) => {
-		try {
-			let { username } = req.query;
-			if(!username || username == "") username = "EpicGamer007313";
-			const data = await mcc[func](username);
-			res.json(data);
-		} catch(err) {
-			console.log(err);
-			res.json({err});
-		}
+router.get("/:game/:func", async (req, res, next) => {
+	const { game, func } = req.params;
+	if(!game || !func || func == "images" || !halo[game] || !halo[game][func]) return next();
+
+	try {
+		const data = await halo[game][func](req.query.username);
+		res.json(data);
+	} catch(err) {
+		console.log(err);
+		res.json(err);
 	}
-	res.mccImages = async (funcWithS = req.path.substring(1)) => {
 
-		try {
-			let { username } = req.query;
-			if(!username || username == "") username = "EpicGamer007313";
+});
 
-			const tks = funcWithS.split("/");
-			const func = tks[tks.length - 1];
+router.get("/:game/images/:func", async (req, res, next) => {
+	const { game, func } = req.params;
+	if(!game || !func || !halo[game] || !halo[game].images || !halo[game].images[func]) return next();
 
-			const data = await mcc.images[func](username);
-			res.set("Content-Type", "image/jpeg");
-			data.pipe(res);
+	try {
 
-		} catch(err) {
-			console.log(err);
-			res.json({err});
-		}
+		const data = await halo[game].images[func](req.query.username);
+		if(typeof data === "string") return res.redirect(data);
+		res.set("Content-Type", "image/jpeg");
+		data.pipe(res);
+		
+	} catch(err) {
+		console.log(err);
+		res.json(err);
 	}
-	next();
-});
 
-router.get("/stats", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/ranks", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/matches", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/xp", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/appearance", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/clips", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/screenshots", (req, res) => {
-	res.mccJson();
-});
-
-router.get("/xp", (req, res) => {
-	res.mccJson()
-});
-
-router.get("/appearance", (req, res) => {
-	res.mccJson()
-});
-
-router.get("/images/xp", (req, res) => {
-	res.mccImages();
-});
-
-router.get("/images/ranks", async (req, res) => {
-	res.mccImages();
 });
 
 export default router;
